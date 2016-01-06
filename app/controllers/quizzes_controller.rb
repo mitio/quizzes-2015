05cae364c83eff5d821713945118cc5f371b57e7
Dimitar Dimitrov <me@ddimitrov.name>
2016-01-06 20:48:25 +0200

    Add a form to create quizzes

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -2,4 +2,24 @@ class QuizzesController < ApplicationController
   def index
     @quizzes = Quiz.all.order(:name)
   end
+
+  def new
+    @quiz = Quiz.new
+  end
+
+  def create
+    @quiz = Quiz.create(quiz_params)
+
+    if @quiz.persisted?
+      redirect_to quizzes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def quiz_params
+    params.require(:quiz).permit(:name)
+  end
 end
